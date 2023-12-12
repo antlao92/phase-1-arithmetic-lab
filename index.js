3e9d2ba9c949e2ce44d1eb83e79d224f80113828
Anthony <anthonykimlao@gmail.com>
2023-12-11 22:51:02 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// multiply variable
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+// random variable
+const random= Math.floor(Math.random() * 10)+ (1);
+
+// mod variable remainder
+const num3 = 16;
+const num4 = 6;
+const mod = (num3 % num4);;
+
+// max variable
+const max = Math.max(1, 2, 3, 4, 20);
